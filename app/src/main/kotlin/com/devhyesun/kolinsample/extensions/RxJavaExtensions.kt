85e9caacfab3ff049f481131b92379d71a3dd5f4
hyesun <hyesun.shin@kt.com>
2019-01-03 23:52:50 +0900

    - 생명주기에 맞는 disposable 관리

--- app/src/main/kotlin/com/devhyesun/kolinsample/extensions/RxJavaExtensions.kt
+++ app/src/main/kotlin/com/devhyesun/kolinsample/extensions/RxJavaExtensions.kt
@@ -1,8 +1,11 @@
 package com.devhyesun.kolinsample.extensions
 
+import com.devhyesun.kolinsample.AutoClearedDisposable
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 
 operator fun CompositeDisposable.plusAssign(disposable: Disposable) {
     this.add(disposable)
 }
+
+operator fun AutoClearedDisposable.plusAssign(disposable: Disposable) = this.add(disposable)
