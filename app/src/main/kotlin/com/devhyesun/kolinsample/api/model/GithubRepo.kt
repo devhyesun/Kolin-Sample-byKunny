c4668849eaa5267963a89c367c395f6b13954291
hyesun <hyesun.shin@kt.com>
2019-01-07 18:53:00 +0900

    - entity 클래스로 변경

--- app/src/main/kotlin/com/devhyesun/kolinsample/api/model/GithubRepo.kt
+++ app/src/main/kotlin/com/devhyesun/kolinsample/api/model/GithubRepo.kt
@@ -1,11 +1,17 @@
 package com.devhyesun.kolinsample.api.model
 
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Embedded
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity(tableName = "repositories")
 class GithubRepo(
     val name: String,
-    @field:SerializedName("full_name") val fullName: String,
-    val owner: GithubOwner,
+    @field:SerializedName("full_name")
+    @PrimaryKey @ColumnInfo(name = "full_name") val fullName: String,
+    @Embedded val owner: GithubOwner,
     val description: String?,
     val language: String?,
     @field:SerializedName("updated_at") val updateAt: String,
