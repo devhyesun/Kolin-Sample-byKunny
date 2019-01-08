d487ad53cfaa873594ff2443837b2c87c149fc2c
hyesun <hyesun.shin@kt.com>
2019-01-08 18:37:03 +0900

    - Database Provider 수정

--- app/src/main/kotlin/com/devhyesun/kolinsample/data/DatabaseProvider.kt
+++ app/src/main/kotlin/com/devhyesun/kolinsample/data/DatabaseProvider.kt
@@ -3,18 +3,16 @@ package com.devhyesun.kolinsample.data
 import android.arch.persistence.room.Room
 import android.content.Context
 
-class DatabaseProvider {
-    private var instance: KolinSampleDatabase? = null
+private var instance: KolinSampleDatabase? = null
 
-    fun provideSearchHistoryDao(context: Context): SearchHistoryDao = providerDatabase(context).searchHistoryDao()
+fun providerSearchHistoryDao(context: Context): SearchHistoryDao = providerDatabase(context).searchHistoryDao()
 
-    private fun providerDatabase(context: Context): KolinSampleDatabase {
-        if (instance == null) {
-            instance =
-                    Room.databaseBuilder(context.applicationContext, KolinSampleDatabase::class.java, "kolin_sample.db")
-                        .build()
-        }
-
-        return instance!!
+private fun providerDatabase(context: Context): KolinSampleDatabase {
+    if (instance == null) {
+        instance =
+                Room.databaseBuilder(context.applicationContext, KolinSampleDatabase::class.java, "kolin_sample.db")
+                    .build()
     }
+
+    return instance!!
 }
